ab14db2f4fc15c4f69f7edc5fa8d35e1cdc794d2
Rachel Moser <moser.rachel@gmail.com>
2021-03-31 20:39:24 -0500

    Add form and images from user name submission

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,20 @@ class StaticPagesController < ApplicationController
   require 'flickr'
 
   def index
-    flickr = Flickr.new ENV['flickr_key'], ENV['flicker_secret']
-    @picture = flickr.photos.getInfo(:photo_id => "3839885270")
+    return if photo_params.empty?
+
+    begin
+      flickr = Flickr.new ENV['flickr_key'], ENV['flicker_secret']
+      @photos = flickr.photos.search(user_id: photo_params[:flickr_id])
+    rescue StandardError => e
+      flash[:alert] = "#{e.message}. So please try a different one."
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def photo_params
+    params.permit(:flickr_id)
   end
 end
